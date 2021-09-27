fd0147acfbd5e8324a3f5731c458e3d461ce8964
Rafael Arredondo <r4f4al@gmail.com>
2021-09-27 13:47:44 -0500

    especifico el encoding del csv

--- vizdiputadostotal.R
+++ vizdiputadostotal.R
@@ -1,9 +1,8 @@
 library(ggplot2)
 library(dplyr)
 #Cargar datos
-diputadostotal <- read.csv("total1.csv",header = TRUE, sep = ",")
-viz1<- ggplot(diputadostotal, aes(Diputados, Partido, fill= Legislatura)) + 
+diputadostotal <- read.csv("total1.csv",header = TRUE, sep = ",", fileEncoding="ISO-8859-1")
+viz1<- ggplot(diputadostotal, aes(Diputados, Partido, fill= Legislatura)) +
   geom_col(position = "dodge2") + coord_flip()
 
 viz1 + theme_classic() + ggtitle("Total diputados por partido en cada Legislatura")
-
