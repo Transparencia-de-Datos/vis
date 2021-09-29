a2b86d54b02971f79c2f1b8fe0e01c494f4edc98
Rafael Arredondo <r4f4al@gmail.com>
2021-09-29 15:13:41 -0500

    cambio de tema, agrega grid

--- viz3.R
+++ viz3.R
@@ -4,10 +4,10 @@ library(tidyr)
 
 df <- read.csv("datos/rp.csv", na.strings = "")
 df1<- df %>%
-  gather("Legislatura","RepresentacionProporcional", 2:4) 
+  gather("Legislatura","RepresentacionProporcional", 2:4)
 
-viz3<- ggplot(df1, aes(x=reorder(Partido, -RepresentacionProporcional), RepresentacionProporcional, fill = Legislatura)) + 
-  geom_col(position="dodge2") 
-viz3 + theme_classic() + labs(title="Distribución de la Representación Proporcional por partidos", subtitle ="Legislaturas LXIII,LXIV y LXV",
+viz3<- ggplot(df1, aes(x=reorder(Partido, -RepresentacionProporcional), RepresentacionProporcional, fill = Legislatura)) +
+  geom_col(position="dodge2")
+viz3 + theme_bw() + labs(title="Distribución de la Representación Proporcional por partidos", subtitle ="Legislaturas LXIII,LXIV y LXV",
                                 caption = "Fuente: SIL") + theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))+
   xlab("Partidos") + ylab("Cantidad de diputados")
