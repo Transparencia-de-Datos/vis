437c34e141c78c2aa600a0dd2927992295e3d475
Rafael Arredondo <r4f4al@gmail.com>
2021-09-29 09:36:47 -0500

    limpio los csv que tenían comas de más y arreglo viz2

--- viz2.R
+++ viz2.R
@@ -5,17 +5,15 @@ library(tidyr)
 df <- read.csv("datos/mr.csv", na.strings = "")
 str(df)
 
-drops  <- c("Eleccion.2018", " Eleccion.2021")
+drops  <- c("Eleccion.2018", "Eleccion.2021")
 df  <- df[, !(names(df) %in% drops)]
 
-#Revisar transformación de los datos, no logré eliminar las 
+#Revisar transformación de los datos, no logré eliminar las
 #columnas que incluían el porcentaje de cambios entre legislaturas
 
 df1<- df %>%
-  gather("Legislatura","MayoriaRelativa",-X) 
+  gather("Legislatura","MayoriaRelativa",-Partido)
 
 
-fig  <- ggplot(df1, aes( x = X, MayoriaRelativa, fill=Legislatura)) + geom_col(position = "dodge2")
-fig + theme_classic() + labs(title="Distribución de la Mayoría Relativa por partidos", subtitle ="Legislaturas LXIII,LXIV y LXV",
-                             caption = "Fuente: SIL")
-
+fig  <- ggplot(df1, aes( x = Partido, MayoriaRelativa, fill=Legislatura)) + geom_col(position = "dodge2")
+fig + theme_classic() + labs(title="Distribución de la Mayoría Relativa por partidos", subtitle ="Legislaturas LXIII,LXIV y LXV", caption = "Fuente: SIL")
