1de635d1d9ec757cf928486be7788185a5642362
Michell Hernández <79372966+Michell2hdz@users.noreply.github.com>
2021-09-29 08:38:48 -0500

    gráfica mayoría relativa (primer intento)
    Sobran dos columnas en la gráfica. Revisar uso de función gather

---
+++ viz2.R
@@ -0,0 +1,21 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+df <- read.csv("datos/mr.csv", na.strings = "")
+str(df)
+
+drops  <- c("Eleccion.2018", " Eleccion.2021")
+df  <- df[, !(names(df) %in% drops)]
+
+#Revisar transformación de los datos, no logré eliminar las 
+#columnas que incluían el porcentaje de cambios entre legislaturas
+
+df1<- df %>%
+  gather("Legislatura","MayoriaRelativa",-X) 
+
+
+fig  <- ggplot(df1, aes( x = X, MayoriaRelativa, fill=Legislatura)) + geom_col(position = "dodge2")
+fig + theme_classic() + labs(title="Distribución de la Mayoría Relativa por partidos", subtitle ="Legislaturas LXIII,LXIV y LXV",
+                             caption = "Fuente: SIL")
+
