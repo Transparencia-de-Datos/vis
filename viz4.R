d1ae33ba665a303e53bb109ba979ce1ca8cdff09
Michell Hernández <79372966+Michell2hdz@users.noreply.github.com>
2021-09-30 15:07:36 -0500

    modificación estética
    Revisar orientación de las barras

--- viz4.R
+++ viz4.R
@@ -1,12 +1,16 @@
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(tidyverse)
 
 df <- read.csv("datos/total.csv", na.strings = "")
 df1 <- df %>% select(Partido, Cambio.Eleccion.2018, Cambio.Eleccion.2021) %>%
-  gather("Cambio", "Cantidad", 2:3)
+  gather("Periodo", "CambioPorcentual", 2:3) %>%
+  mutate(pos = CambioPorcentual >= 0) %>%
+  arrange(CambioPorcentual)
+  
+viz4<-ggplot(df1, aes(x= Partido,y=CambioPorcentual)) + 
+  geom_bar(stat = "identity",  aes(fill=pos))
+viz4 + theme_bw() +  coord_flip() + facet_grid(~Periodo) + 
+  labs(title ="Cambio porcentual en cantidad de diputados totales", subtitle="Elecciones 2018 y 2021", caption = "Fuente: SIL")
 
-
-viz4<-ggplot(df1, aes(Partido, Cantidad, fill = Cambio)) + 
-  geom_col(position = "dodge2")
-viz4 + theme_bw() + labs("Cambio porcentual por partido", "legislaturas LXIV y LXV", caption = "Fuente: SIL")
