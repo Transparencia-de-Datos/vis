8234ece3fa48876e859e2bfcb735684ebe0633c3
Michell Hernández <79372966+Michell2hdz@users.noreply.github.com>
2021-10-06 12:33:02 -0500

    Cambio nombre leyendas

--- fig1.r
+++ fig1.r
@@ -19,5 +19,6 @@ g_pic  <- rasterGrob(img, interpolate = TRUE)
 fig  <- ggplot(df1, aes( x = reorder(Partido, -Diputados), Diputados, fill=Legislatura)) + geom_col(position = "dodge2")
 fig + theme_classic() + labs(title="Total de diputados por partido en cada Legislatura", subtitle ="Legislaturas LXIII,LXIV y LXV", caption = "Fuente: Currícula de la Cámara de Diputados") +
   xlab("Partidos") + ylab("Cantidad de diputados") + theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) + annotation_custom(g_pic, xmin=5, xmax=Inf, ymin=5, ymax=Inf)+
-  theme(legend.position = "top") + geom_text(aes(label = Diputados),  position = position_dodge(width = 1), vjust = -.4)
+  theme(legend.position = "top") + geom_text(aes(label = Diputados),  position = position_dodge(width = 1), vjust = -.4) + 
+  scale_fill_discrete(labels = c("Legislatura 2015-2018", "Legislatura 2018-2021", "Legislatura 2021-2024"))
 
