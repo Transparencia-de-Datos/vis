7afcb5181d8c04c614f7df6402a88aa4ab596918
Michell Hernández <79372966+Michell2hdz@users.noreply.github.com>
2021-10-06 12:32:20 -0500

    Cambio nombre de las leyendas

--- viz3.R
+++ viz3.R
@@ -15,7 +15,8 @@ viz3<- ggplot(df1, aes(x=reorder(Partido, -RepresentacionProporcional), Represen
   geom_col(position="dodge2")
 viz3 + theme_classic() + labs(title="Distribución de la Representación Proporcional por partidos", subtitle ="Legislaturas LXIII,LXIV y LXV",
                                 caption = "Fuente: Currícula Cámara de Diputados") + theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))+
-  xlab("Partidos") + ylab("Cantidad de diputados") + theme(legend.position = "top") + geom_text(aes(label = RepresentacionProporcional),  position = position_dodge(width = 1), vjust = -.4)
+  xlab("Partidos") + ylab("Cantidad de diputados") + theme(legend.position = "top") + geom_text(aes(label = RepresentacionProporcional),  position = position_dodge(width = 1), vjust = -.4)+
+  scale_fill_discrete(labels = c("Legislatura 2015-2018", "Legislatura 2018-2021", "Legislatura 2021-2024"))
 
 
 
--- viz4.R
+++ viz4.R
@@ -7,13 +7,13 @@ df <- read.csv("datos/total.csv", na.strings = "")
 df1 <- df %>% select(Partido, Cambio.Eleccion.2018, Cambio.Eleccion.2021) %>%
   gather("Periodo", "CambioPorcentual", 2:3) %>%
   mutate(Cambio = as.numeric(sub("%","",CambioPorcentual))) %>%
-  arrange(Cambio)
+  arrange(Cambio) %>%
+filter(Partido != "NA")
   
-viz4<-ggplot(df1, aes(x= Partido,y=Cambio)) + ylim(-80, 410) +
+viz4<-ggplot(df1, aes(x= Partido,y=Cambio)) + ylim(-120, 410) +
   geom_bar(stat = 'identity',  aes(fill= Partido))
 viz4 + theme_classic() +  coord_flip() + facet_grid(~Periodo) + 
   labs(title ="Cambio porcentual en cantidad de diputados totales", subtitle="Elecciones 2018 y 2021", caption = "Fuente: Currícula Cámara de Diputados")+
-   geom_text(aes(label = Cambio))
-
+   geom_text(aes(label = Cambio)) + theme(legend.position = "top")
 
 
