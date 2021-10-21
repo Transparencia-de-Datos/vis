d707dde5bef039272f9e6d34e2f0c404843ab53e
Michell Hernández <79372966+Michell2hdz@users.noreply.github.com>
2021-10-21 18:06:11 -0500

    Cambio de nombre de los archivos por error

---
+++ viz_generaciones65_.R
@@ -0,0 +1,17 @@
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(ggsci)
+
+df <- read_excel("datos/Generaciones_2018_2021.xlsx",sheet = 6)
+df1<- df %>%
+  select( tipoEleccion ,Silenciosa,Boomers,GenX,Millenials, Z) %>%
+  gather("Generacion", "Total", 2:6) %>%
+  filter(Total != 0)
+
+viz1 <- ggplot(df1, aes(tipoEleccion, Total, fill=Generacion)) + geom_col(position="dodge")
+viz1 + theme_classic() + labs(title="Distribución generacional diputados por tipo de elección", subtitle ="Legislaturas LXIV", caption = "Fuente: Currícula de la Cámara de Diputados") +
+  theme(legend.position = "top") + geom_text(aes(label = Total),  position = position_dodge(width = 1), vjust = -.4) + 
+  scale_fill_jama()+ scale_y_continuous(limits=c(0, 150)) + xlab("Tipo de elección")
+
--- viz_generaciones65_partido.R
+++ viz_generaciones65_partido.R
@@ -4,14 +4,14 @@ library(tidyr)
 library(ggplot2)
 library(ggsci)
 
-df <- read_excel("datos/Generaciones_2018_2021.xlsx",sheet = 6)
+df <- read_excel("datos/Generaciones_2018_2021.xlsx",sheet = 4)
 df1<- df %>%
-  select( tipoEleccion ,Silenciosa,Boomers,GenX,Millenials, Z) %>%
+  select( Partido ,Silenciosa,Boomers,GenX,Millenials, Z) %>%
   gather("Generacion", "Total", 2:6) %>%
-  filter(Total != 0)
+  filter(Total != 0) %>%
+  filter(Partido != "Totales")
 
-viz1 <- ggplot(df1, aes(tipoEleccion, Total, fill=Generacion)) + geom_col(position="dodge")
-viz1 + theme_classic() + labs(title="Distribución generacional diputados por tipo de elección", subtitle ="Legislaturas LXIV", caption = "Fuente: Currícula de la Cámara de Diputados") +
-  theme(legend.position = "top") + geom_text(aes(label = Total),  position = position_dodge(width = 1), vjust = -.4) + 
-  scale_fill_jama()+ scale_y_continuous(limits=c(0, 150)) + xlab("Tipo de elección")
-
+viz <- ggplot(df1, aes(Partido, Total, fill=Generacion)) + geom_col(position="dodge2")
+viz + theme_classic() + labs(title="Distribución generacional de los diputados por partido", subtitle ="Legislatura LXV", caption = "Fuente: Currícula de la Cámara de Diputados") +
+  theme(legend.position = "top") + geom_text(aes(label = Total),  position = position_dodge(width = 1), vjust = -.4)+
+  scale_y_continuous(limits=c(0, 100)) + scale_fill_aaas()
