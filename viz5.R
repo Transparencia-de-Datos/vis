3b41b5124810058d36385b73263210247f3b1c9b
Michell Hernández <79372966+Michell2hdz@users.noreply.github.com>
2021-10-21 17:45:46 -0500

    Update viz5.R

--- viz5.R
+++ viz5.R
@@ -4,6 +4,7 @@ library(tidyr)
 library(png)
 library(grid)
 library(ggsci)
+library(car)
 
 img <- png::readPNG("img/logo.png")
 g_pic  <- rasterGrob(img, interpolate = TRUE)
@@ -11,14 +12,16 @@ g_pic  <- rasterGrob(img, interpolate = TRUE)
 df <- read.csv("datos/gubernaturas.csv", na.strings = "")
 df1 <- df %>%
   gather("Elecciones", "Cantidad", 2:3) %>%
-  filter(Cantidad != 0)
+  filter(Cantidad != 0) %>%
+  mutate(Elecciones= recode(Elecciones, "'Elecciones.2018'='Elecciones 2018'")) %>%
+  mutate(Elecciones= recode(Elecciones, "'Elecciones.2021'='Elecciones 2021'"))
 
-viz5 <- ggplot(df1, aes(reorder(Partido,-Cantidad), Cantidad, fill = Elecciones)) + 
-  geom_col(position = "dodge")
+viz5 <- ggplot(df1, aes(reorder(Partido,-Cantidad), Cantidad, fill= Partido)) + 
+  geom_col() + facet_grid(~Elecciones)
 viz5+ theme_classic() + labs(title ="Cambio de gubernatura por partido", subtitle="Elecciones 2018 y 2021", caption = "Fuente: Currícula de la Cámara de Diputados")+
-  theme(legend.position = "top")+ geom_text(aes(label = Cantidad),  position = position_dodge(width = 1), vjust = -.5) +
-  annotation_custom(g_pic, xmin=5, xmax=Inf, ymin=5, ymax=Inf) + scale_y_continuous(limits=c(0, 20)) +
-  theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) + scale_fill_futurama( labels = c("Elecciones 2018", "Elecciones 2021")) + 
+   geom_text(aes(label = Cantidad),  position = position_dodge(width = 1), vjust = -.5) +
+  annotation_custom(g_pic, xmin=5, xmax=Inf, ymin=5, ymax=Inf) + scale_y_continuous(limits=c(0, 16),breaks=seq(0,16,by=2)) +
+  theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5), legend.position = "none") + scale_fill_d3() + 
   xlab("Partidos")
 
 
