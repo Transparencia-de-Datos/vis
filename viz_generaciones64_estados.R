d2edf7370c3934f773a17ea36a1376de87b38f61
Michell Hernández <79372966+Michell2hdz@users.noreply.github.com>
2021-10-21 17:10:13 -0500

    Barras apiladas

--- viz_generaciones64_estados.R
+++ viz_generaciones64_estados.R
@@ -2,13 +2,16 @@ library(readxl)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(ggsci)
 
 df <- read_excel("datos/Generaciones_2018_2021.xlsx",sheet = 2)
 df1<- df %>%
   select( estado ,Silenciosa,Boomers,GenX,Millenials, Z) %>%
-  gather("Generacion", "Total", 2:6) 
+  gather("Generacion", "Total", 2:6) %>%
+  filter(Total != 0)
 
-viz <- ggplot(df1, aes(estado, Total, fill=Generacion)) + geom_col(position="dodge2")
+viz <- ggplot(df1, aes(reorder(estado,Total), Total, fill=Generacion)) + geom_col()
 viz + coord_flip() + theme_classic() + labs(title="Distribución generacional de los diputados por estados", subtitle ="Legislaturas LXIV", caption = "Fuente: Currícula de la Cámara de Diputados")+
-  theme(legend.position = "top") + geom_text(aes(label = Total),  position = position_dodge(width = 1), vjust = -.4)
+  theme(legend.position = "top") + geom_text(aes(label = Total),  position = position_dodge(width = 1), vjust = -.4) + 
+  scale_fill_lancet() 
 
--- viz_generaciones65_estados.R
+++ viz_generaciones65_estados.R
@@ -2,6 +2,7 @@ library(readxl)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(ggsci)
 
 df <- read_excel("datos/Generaciones_2018_2021.xlsx",sheet = 5)
 df1<- df %>%
@@ -9,8 +10,8 @@ df1<- df %>%
   gather("Generacion", "Total", 2:6) %>%
   filter(Total != 0)
 
-viz <- ggplot(df1, aes(estado, Total, fill=Generacion)) + geom_col(position="dodge2")
+viz <- ggplot(df1, aes(reorder(estado,Total), Total, fill=Generacion)) + geom_col()
 viz + theme_classic() + labs(title="Distribución generacional de los diputados por estado", subtitle ="Legislatura LXV", caption = "Fuente: Currícula de la Cámara de Diputados") +
   theme(legend.position = "top") + geom_text(aes(label = Total),  position = position_dodge(width = 1), vjust = -.4) +
-  coord_flip()
+  coord_flip() + scale_fill_lancet()
 
