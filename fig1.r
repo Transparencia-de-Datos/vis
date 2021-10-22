a2c46d8ccc601a86b97380f01a0876080030e48a
Michell Hernández <79372966+Michell2hdz@users.noreply.github.com>
2021-10-21 21:29:51 -0500

    cambio de títulos

--- fig1.r
+++ fig1.r
@@ -19,7 +19,7 @@ img <- png::readPNG("img/logo.png")
 g_pic  <- rasterGrob(img, interpolate = TRUE)
 
 fig  <- ggplot(df1, aes( x = reorder(Partido, -Diputados), Diputados, fill=Legislatura)) + geom_col(position = "dodge2")
-fig + theme_classic() + labs(title="Total de diputados por partido en cada Legislatura", subtitle ="Legislaturas LXIII,LXIV y LXV", caption = "Fuente: Currícula de la Cámara de Diputados") +
+fig + theme_classic() + labs(title="Distribución de la totalidad diputados por partido", subtitle ="Legislaturas LXIII,LXIV y LXV", caption = "Fuente: Currícula de la Cámara de Diputados") +
   xlab("Partidos") + ylab("Cantidad de diputados") + theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) + annotation_custom(g_pic, xmin=5, xmax=Inf, ymin=5, ymax=Inf)+
   theme(legend.position = "top") + scale_y_continuous(limits=c(0, 300))+ geom_text(aes(label = Diputados),  position = position_dodge(width = 0.9), vjust = -.5) + 
   scale_fill_jama(labels = c("Legislatura 2015-2018", "Legislatura 2018-2021", "Legislatura 2021-2024"))
--- viz3.R
+++ viz3.R
@@ -15,7 +15,7 @@ g_pic  <- rasterGrob(img, interpolate = TRUE)
 
 viz3<- ggplot(df1, aes(x=reorder(Partido, -RepresentacionProporcional), RepresentacionProporcional, fill = Legislatura)) +
   geom_col(position="dodge2")
-viz3 + theme_classic() + labs(title="Distribución de la Representación Proporcional por partidos", subtitle ="Legislaturas LXIII,LXIV y LXV",
+viz3 + theme_classic() + labs(title="Distribución de la representación proporcional por partidos", subtitle ="Legislaturas LXIII,LXIV y LXV",
                                 caption = "Fuente: Currícula Cámara de Diputados") + theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))+
   xlab("Partidos") + ylab("Cantidad de diputados") + theme(legend.position = "top") + geom_text(aes(label = RepresentacionProporcional),  position = position_dodge(width = 1), vjust = -.5)+ scale_y_continuous(limits=c(0, 100))+
     annotation_custom(g_pic, xmin=5, xmax=Inf, ymin=5, ymax=Inf) + scale_fill_jama(labels = c("Legislatura 2015-2018", "Legislatura 2018-2021", "Legislatura 2021-2024"))
