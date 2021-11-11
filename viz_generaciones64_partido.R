b188910458f3a8d8692b53d52c3ddafadcf3b18f
Michell Hernández <79372966+Michell2hdz@users.noreply.github.com>
2021-11-11 15:45:26 -0600

    Inclusión de logo

--- viz_generaciones64_partido.R
+++ viz_generaciones64_partido.R
@@ -4,6 +4,7 @@ library(tidyr)
 library(ggplot2)
 library(forcats)
 library(ggsci)
+library(png)
 
 df <- read_excel("datos/Generaciones_2018_2021.xlsx",sheet = 3)
 df1<- df %>%
@@ -12,10 +13,16 @@ df1<- df %>%
   gather("Generacion", "Total", 2:6) %>%
   filter(Total != 0) %>%
   mutate(Generacion = fct_reorder(Generacion,desc(Total))) %>%
-  mutate(Partido = fct_reorder(Partido,desc(Total))) 
+  mutate(Partido = fct_reorder(Partido,desc(Total)))  %>%
+  group_by(Generacion) %>%
+  mutate(propor = Total / sum(Total) ) %>%
+  mutate(propor = round(propor, 2))
+
+img <- png::readPNG("img/logo.png")
+g_pic  <- rasterGrob(img, interpolate = TRUE)
 
 viz <- ggplot(df1, aes(Partido, Total, fill=Generacion)) + geom_col(position="dodge2")
 viz + theme_classic() + labs(title="Distribución generacional de los diputados por partido", subtitle ="Legislatura LXIV", caption = "Fuente: Currícula de la Cámara de Diputados") +
   theme(legend.position = "top") + geom_text(aes(label = Total),  position = position_dodge(width = 1), vjust = -.4) +
-  scale_fill_jama() + scale_y_continuous(limits=c(0, 120))
+  scale_fill_jama() + scale_y_continuous(limits=c(0, 120)) + annotation_custom(g_pic, xmin=5, xmax=Inf, ymin=5, ymax=Inf)
 
--- viz_generaciones65_partido.R
+++ viz_generaciones65_partido.R
@@ -3,6 +3,8 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(ggsci)
+library(png)
+library(grid)
 
 df <- read_excel("datos/Generaciones_2018_2021.xlsx",sheet = 4)
 df1<- df %>%
@@ -11,9 +13,17 @@ df1<- df %>%
   filter(Total != 0) %>%
   filter(Partido != "Totales") %>%
   mutate(Generacion = fct_reorder(Generacion,desc(Total))) %>%
-  mutate(Partido = fct_reorder(Partido,desc(Total))) 
+  mutate(Partido = fct_reorder(Partido,desc(Total)))%>%
+  group_by(Generacion) %>%
+  mutate(propor = Total / sum(Total) ) %>%
+  mutate(propor = round(propor, 2))
+
+img <- png::readPNG("img/logo.png")
+g_pic  <- rasterGrob(img, interpolate = TRUE)
 
 viz <- ggplot(df1, aes(Partido, Total, fill=Generacion)) + geom_col(position="dodge2")
 viz + theme_classic() + labs(title="Distribución generacional de los diputados por partido", subtitle ="Legislatura LXV", caption = "Fuente: Currícula de la Cámara de Diputados") +
   theme(legend.position = "top") + geom_text(aes(label = Total),  position = position_dodge(width = 1), vjust = -.4)+
-  scale_y_continuous(limits=c(0, 100)) + scale_fill_jama()
+  scale_y_continuous(limits=c(0, 100)) + scale_fill_jama() + annotation_custom(g_pic, xmin=5, xmax=Inf, ymin=5, ymax=Inf)
+
+
