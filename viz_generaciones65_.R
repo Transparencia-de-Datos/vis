051e262748b20535d1a2d6749e36570681b4c6ec
Michell Hernández <79372966+Michell2hdz@users.noreply.github.com>
2021-11-11 15:50:26 -0600

    Agregar logo (revisar aún no es visible)

--- viz_generaciones65_.R
+++ viz_generaciones65_.R
@@ -13,8 +13,12 @@ df1<- df %>%
   filter(Total != 0) %>%
   mutate(Generacion=fct_reorder(Generacion, desc(Total))) 
 
+img <- png::readPNG("img/logo.png")
+g_pic  <- rasterGrob(img, interpolate = TRUE)
+
 viz1 <- ggplot(df1, aes(tipoEleccion, Total, fill=Generacion)) + geom_col(position="dodge")
 viz1 + theme_classic() + labs(title="Distribución generacional diputados por tipo de elección", subtitle ="Legislaturas LXIV", caption = "Fuente: Currícula de la Cámara de Diputados") +
   theme(legend.position = "top") + geom_text(aes(label = Total),  position = position_dodge(width = 1), vjust = -.4) + 
-  scale_fill_jama()+ scale_y_continuous(limits=c(0, 150)) + xlab("Tipo de elección")
+  scale_fill_jama()+ scale_y_continuous(limits=c(0, 150)) + xlab("Tipo de elección")+
+  annotation_custom(g_pic, xmin=5, xmax=Inf, ymin=5, ymax=Inf)
 
