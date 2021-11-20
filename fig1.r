003c21425cea6f0b38ec47bf3a3be4d05c095b6a
Rafael Arredondo <r4f4al@gmail.com>
2021-11-19 18:34:31 -0600

    mejor posicionamiento del logo

--- fig1.r
+++ fig1.r
@@ -22,7 +22,6 @@ g_pic  <- rasterGrob(img, interpolate = TRUE)
 
 fig  <- ggplot(df1, aes(Partido, Diputados, fill=Legislatura)) + geom_col(position = "dodge2")
 fig + theme_classic() + labs(title="Distribución del total de diputados por partido", subtitle ="Legislaturas LXIII,LXIV y LXV", caption = "Fuente: Currícula de la Cámara de Diputados") +
-  xlab("Partidos") + ylab("Cantidad de diputados") + theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) + annotation_custom(g_pic, xmin=5, xmax=Inf, ymin=5, ymax=Inf)+
-  theme(legend.position = "top") + scale_y_continuous(limits=c(0, 300))+ geom_text(aes(label = Diputados),  position = position_dodge(width = 0.9), vjust = -.5) + 
+  xlab("Partidos")+ ylab("Cantidad de diputados") + theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) + annotation_custom(g_pic, xmin=7, xmax=9, ymin=150, ymax=300)+
+  theme(legend.position = "top") + scale_y_continuous(limits=c(0, 300))+ geom_text(aes(label = Diputados),  position = position_dodge(width = 0.9), vjust = -.5) +
   scale_fill_jama(labels = c("Legislatura 2015-2018", "Legislatura 2018-2021", "Legislatura 2021-2024"))
-
