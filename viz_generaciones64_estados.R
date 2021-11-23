3089b7c9b38a7878035377f3b1cdb0ec4e1c1bfa
Michell Hernández <79372966+Michell2hdz@users.noreply.github.com>
2021-11-23 16:57:29 -0600

    Cambio tamaño de letra y título

--- viz_generaciones64_estados.R
+++ viz_generaciones64_estados.R
@@ -24,8 +24,8 @@ df1<- df_porcentaje %>%
   arrange(desc(Porcentaje))
   
 viz <- ggplot(df1, aes(estado, Porcentaje, fill=Generacion)) + geom_col()
-viz + theme_classic() + labs(title="Distribución generacional de los diputados por estados", subtitle ="Legislaturas LXIV", caption = "Fuente: Currícula de la Cámara de Diputados")+
-  theme(legend.position = "top", axis.text.x=element_text(angle = 90, hjust = 1)) + geom_text(aes(label = Porcentaje), color = "white", position = "stack", size = 2, hjust = 0.5, vjust = 1) + 
+viz + theme_classic() + labs(title="Distribución generacional de los diputados por estados", subtitle ="Legislatura LXIV (2018-2021)", caption = "Fuente: Currícula de la Cámara de Diputados")+
+  theme(legend.position = "top", axis.text.x=element_text(angle = 90, hjust = 1)) + geom_text(aes(label = Porcentaje), color = "white", fontface=2, position = "stack", size = 3, hjust = 0.5, vjust = 1) + 
   scale_fill_jama(name = "Generación", labels = c( "X","Boomers", "Millenials", "Silenciosa"))
 
 
--- viz_generaciones65_estados.R
+++ viz_generaciones65_estados.R
@@ -25,13 +25,10 @@ df1<- df_porcentaje %>%
   arrange(desc(Porcentaje))
 
 viz <- ggplot(df1, aes(estado, Porcentaje, fill=Generacion)) + geom_col()
-viz + theme_classic() + labs(title="Porcentaje de los diputados en cada estado por generación", subtitle ="Legislatura LXV", caption = "Fuente: Currícula de la Cámara de Diputados") +
+viz + theme_classic() + labs(title="Distribución generacional de los diputados por estados", subtitle ="Legislatura LXV (2021-2024)", caption = "Fuente: Currícula de la Cámara de Diputados") +
   theme(legend.position = "top", axis.text.x=element_text(angle =90, hjust =1))+
-     geom_text(aes(label = Porcentaje), color = "white" , position = "stack", size = 2, hjust = 0.5, vjust = 1) +
+     geom_text(aes(label = Porcentaje), color = "white", fontface=2, position = "stack", size = 3, hjust = 0.5, vjust = 1) +
    scale_fill_jama(name = "Generación", labels = c("X", "Boomers" ,"Millenials", "Silenciosa", "Z")) 
 
 
-df2<- df1 %>%
-  group_by(estado) %>%
-  summarise(sum(Porcentaje))
 
