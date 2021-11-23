e288ac8b8e6bdeae9ff9aaf89c8525081171741f
Michell Hernández <79372966+Michell2hdz@users.noreply.github.com>
2021-11-23 16:24:51 -0600

    Modificación etiquetas

--- viz_generaciones64_estados.R
+++ viz_generaciones64_estados.R
@@ -9,10 +9,10 @@ df <- read_excel("datos/Generaciones_2018_2021.xlsx",sheet = 2)
 df_porcentaje <- df %>%
   mutate(silenciosa_porcentaje = Silenciosa / Total) %>%
   mutate(boomers_porcentaje = Boomers / Total) %>%
-  mutate(genX_porcentaje = GenX / Total) %>%
+  mutate(agenX_porcentaje = GenX / Total) %>%
   mutate(millenials_porcentaje = Millenials / Total) %>%
   mutate(z_porcentaje = Z/Total)  %>%
- select(estado, silenciosa_porcentaje, boomers_porcentaje, genX_porcentaje,
+ select(estado, silenciosa_porcentaje, boomers_porcentaje, agenX_porcentaje,
         millenials_porcentaje, z_porcentaje)
 
 df1<- df_porcentaje %>%
@@ -27,7 +27,7 @@ df1<- df_porcentaje %>%
 viz <- ggplot(df1, aes(estado, Porcentaje, fill=Generacion)) + geom_col()
 viz + theme_classic() + labs(title="Distribución generacional de los diputados por estados", subtitle ="Legislaturas LXIV", caption = "Fuente: Currícula de la Cámara de Diputados")+
   theme(legend.position = "top", axis.text.x=element_text(angle = 90, hjust = 1)) + geom_text(aes(label = Porcentaje), color = "white", position = "stack", size = 2, hjust = 0.5, vjust = 1) + 
-  scale_fill_jama(name = "Generación", labels = c("Boomers", "X", "Millenial", "Silenciosa"))
+  scale_fill_jama(name = "Generación", labels = c( "X","Boomers", "Millenials", "Silenciosa"))
 
 df2<- df1 %>%
   group_by(estado) %>%
--- viz_generaciones65_estados.R
+++ viz_generaciones65_estados.R
@@ -8,21 +8,25 @@ df <- read_excel("datos/Generaciones_2018_2021.xlsx",sheet = 5)
 df_porcentaje <- df %>%
   mutate(silenciosa_porcentaje = Silenciosa / Total) %>%
   mutate(boomers_porcentaje = Boomers / Total) %>%
-  mutate(genX_porcentaje = GenX / Total) %>%
+  mutate(agenX_porcentaje = GenX / Total) %>%
   mutate(millenials_porcentaje = Millenials / Total) %>%
   mutate(z_porcentaje = Z/Total)  %>%
-  select(estado, silenciosa_porcentaje, boomers_porcentaje, genX_porcentaje,
+  select(estado, silenciosa_porcentaje, boomers_porcentaje, agenX_porcentaje,
          millenials_porcentaje, z_porcentaje)
 
 df1<- df_porcentaje %>%
   gather("Generacion", "Proporcion", 2:6) %>%
   filter(Proporcion != 0) %>%
   mutate(Proporcion = round(Proporcion, 3)) %>%
-  mutate(Porcentaje = Proporcion *100)
+  mutate(Porcentaje = Proporcion *100) %>%
+  arrange(desc(Porcentaje))
 
 viz <- ggplot(df1, aes(estado, Porcentaje, fill=Generacion)) + geom_col()
 viz + theme_classic() + labs(title="Porcentaje de los diputados en cada estado por generación", subtitle ="Legislatura LXV", caption = "Fuente: Currícula de la Cámara de Diputados") +
   theme(legend.position = "top", axis.text.x=element_text(angle =90, hjust =1))+
      geom_text(aes(label = Porcentaje), color = "white" , position = "stack", size = 2, hjust = 0.5, vjust = 1) +
-   scale_fill_jama(name = "Generación", labels = c("Boomers", "X", "Millenial", "Silenciosa", "Z"))
+   scale_fill_jama(name = "Generación", labels = c("X", "Boomers" ,"Millenials", "Silenciosa", "Z"))
 
+df2<- df1 %>%
+  group_by(estado) %>%
+  summarise(sum(Porcentaje))
